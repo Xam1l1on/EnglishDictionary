6dd368680357e72a8f5770e4b68ed3194636574a
kirso <kirso@DESKTOP-TEH05FF>
2021-07-25 22:10:27 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EnglishDictionary.ViewModels.Base
+{
+    class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        #region Fields
+        private bool _disposed;
+        #endregion
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChangeed([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Setter Wrappers
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChangeed(propertyName);
+            return true;
+        }
+        #endregion
+        #region Dispose
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposing || _disposed)
+            {
+                return;
+            }
+            _disposed = true;
+        }
+        #endregion
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnglishDictionary.ViewModels.Base;
+
+namespace EnglishDictionary.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Fileds
+        private string _title;
+        private string _statusBar;
+
+        #endregion
+        #region Properties
+        /// <summary>Свойство заголовка окна</summary>
+        public string Title { get => _title; private set => Set(ref _title, value); }
+        public string StatusBar { get => _statusBar; set => Set(ref _statusBar, value); }
+        #endregion
+    }
+}
